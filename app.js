4331b941963fe80648004a94aeddc4ddfb65ac85
AARON M <asmarcks@gmail.com>
2019-12-19 22:59:13 -0600

    correct answer function

---
+++ app.js
@@ -0,0 +1,17 @@
+function question(correctAnswer) {
+    if (correctAnswer === '40km/h' || 40) {
+        console.log('correct')
+    }else {
+        console.log('incorrect')
+    }
+    
+};
+
+
+
+
+    // if(questionOne.correctAnswer === '40mph'){
+    //     console.log('correct')
+    // }else{
+    //     console.log('incorrect')
+    // };
